b95677939d43868c3eb45986daff7a4370c9079f
Rusver Alves <rusverav748@gmail.com>
2023-02-28 17:10:14 -0300

    Projeto Criado

---
+++ Consumer_API/Modelo/Carros.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosumindo_API.Modelo
+{
+    public class Carros
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Marca { get; set; }
+        public string? Placa { get; set; }
+
+    }
+}
---
+++ Consumer_API/Program.cs
@@ -0,0 +1,37 @@
+﻿using Cosumindo_API.Modelo;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+var urlGet = "https://localhost:7002/ListarCarros";
+var urlPost = "https://localhost:7002/AdicionaCarro";
+JsonSerializerOptions options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };//Tanto maiusculas ou minusculas
+
+using (var httpClient = new HttpClient())
+{
+    //Get
+    var respostaGet = await httpClient.GetAsync(urlGet);
+    if (respostaGet.IsSuccessStatusCode)
+    {
+        Console.WriteLine("Busca de dados Projeto_API");
+
+        var contexto = await respostaGet.Content.ReadAsStringAsync();
+        var carros = JsonSerializer.Deserialize<List<Carros>>(contexto, options);
+
+        foreach (var item in carros)
+        {
+            Console.WriteLine($"ID:{item.Id} Nome:{item.Nome} Marca:{item.Marca} Placa:{item.Placa} ");
+        }
+    }
+
+    //// Post
+    //Console.WriteLine("Inserindo dados Projeto_API");
+    //var respostaPost = await httpClient.PostAsJsonAsync(urlPost, new Carros { Nome = "New Prisma", Marca = " Chevrolet", Placa = "PRIS¨9999" });
+
+    //if (respostaPost.IsSuccessStatusCode)
+    //    Console.WriteLine("Carro Adicionado");
+    //else
+    //    Console.WriteLine("Error");
+
+
+    Console.ReadLine();
+}
